b78827d71e54ff05d4f1fcf1bd2c02197b18d1fa
João Paulo Correia <32521535+joaopaulocorreia@users.noreply.github.com>
2023-10-13 19:43:59 -0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,39 +3,20 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
-vim.opt.background = "dark"
+vim.opt.background = "light"
 
 lvim.transparent_window = false
-lvim.colorscheme = "lunar"
--- lvim.colorscheme = "lunaperche"
+lvim.colorscheme = "lunaperche"
+lvim.format_on_save.enabled = true
 
----- Buffer navigation --------------------------------------
 lvim.keys.normal_mode["<S-j>"] = false
 lvim.keys.normal_mode["<S-k>"] = false
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
--------------------------------------------------------------
 
----- ZenMode ------------------------------------------------
 lvim.keys.normal_mode["<c-w>"] = ":ZenMode<CR>"
--------------------------------------------------------------
 
----- Lunaline -----------------------------------------------
-local components = require("lvim.core.lualine.components")
-
-lvim.builtin.lualine.sections.lualine_a = { "mode" }
-lvim.builtin.lualine.sections.lualine_y = {
-  components.spaces,
-  components.location
-}
-lvim.builtin.lualine.style = "none"
-lvim.builtin.lualine.style = "default"
-lvim.builtin.lualine.sections.lualine_c = { "diff" }
--------------------------------------------------------------
-
----- Plugins ------------------------------------------------
 lvim.plugins = {
-  {"lunarvim/colorschemes"},
-  {"folke/zen-mode.nvim"}
+  { "lunarvim/colorschemes" },
+  { "folke/zen-mode.nvim" },
 }
--------------------------------------------------------------
